bd2fde396bc79b2bf6f0451594b0d1c809dcb0ca
Natalie Halawani <hala1224@gmail.com>
2018-11-01 02:30:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -77,6 +77,34 @@ greet.bind(sally)('Bob');
 // billy.matchInterests(freeMusic);
 // // Uncaught TypeError: Cannot read property 'interests' of undefined
 
+// class Event {
+//     constructor(title, keywords) {
+//         this.title = title;
+//         this.keywords = keywords;
+//     }
+// }
+ 
+// class User {
+//     constructor(name, interests) {
+//         this.name = name;
+//         this.interests = interests;
+//     }
+ 
+//     matchInterests(event) {
+//         console.log("'this' is defined: ", this);
+//         return event.keywords.some(function(word) {
+//             console.log("'this' is now undefined: ", this);
+//             return this.interests.includes(word);
+//         });
+//     }
+// }
+ 
+// let billy = new User('billy', ['music', 'art', 'movies']);
+// let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+// billy.matchInterests(freeMusic);
+// // Uncaught TypeError: Cannot read property 'interests' of undefined
+
 class Event {
     constructor(title, keywords) {
         this.title = title;
@@ -91,11 +119,11 @@ class User {
     }
  
     matchInterests(event) {
-        console.log("'this' is defined: ", this);
-        return event.keywords.some(function(word) {
-            console.log("'this' is now undefined: ", this);
-            return this.interests.includes(word);
-        });
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
     }
 }
  
@@ -103,5 +131,3 @@ let billy = new User('billy', ['music', 'art', 'movies']);
 let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
  
 billy.matchInterests(freeMusic);
-// Uncaught TypeError: Cannot read property 'interests' of undefined
-
