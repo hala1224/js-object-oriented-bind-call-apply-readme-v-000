387ee9466a34109bc9946f237a2bc644bf0b9192
Natalie Halawani <hala1224@gmail.com>
2018-11-01 02:23:45 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -23,3 +23,6 @@ greet.call(sally);
  
 greet.apply(sally);
 // my name is Sally, hi!
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
